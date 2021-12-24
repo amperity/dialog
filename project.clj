6e03df053e347cc9f1999de7c489e63cd2752696
Greg Look <greg@mvxcvi.com>
2021-12-24 10:05:25 -0800

    Update dependencies.

--- project.clj
+++ project.clj
@@ -7,19 +7,21 @@
   :dependencies
   [[org.clojure/clojure "1.10.3"]
    [org.clojure/data.json "2.4.0"]
-   [org.slf4j/slf4j-api "1.7.30"]
-   [org.slf4j/jul-to-slf4j "1.7.30"]
-   [org.slf4j/jcl-over-slf4j "1.7.30"]]
+   [org.slf4j/slf4j-api "1.7.32"]
+   [org.slf4j/jul-to-slf4j "1.7.32"]
+   [org.slf4j/jcl-over-slf4j "1.7.32"]
+   [io.aviso/pretty "1.1.1"]
+   [aero "1.1.6"]]
 
   :source-paths ["src/clojure"]
   :java-source-paths ["src/java"]
 
   :profiles
   {:dev
-   {:dependencies [[org.clojure/tools.logging "1.2.2"]]}
+   {:dependencies [[org.clojure/tools.logging "1.2.3"]]}
 
    :repl
    {:source-paths ["dev"]
     :repl-options {:init-ns dialog.repl}
-    :dependencies [[org.clojure/tools.namespace "1.1.0"]]
+    :dependencies [[org.clojure/tools.namespace "1.2.0"]]
     :jvm-opts ["-XX:-OmitStackTraceInFastThrow"]}})
