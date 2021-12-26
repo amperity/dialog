fc6f5b2d7cfb903b3af272c13ee24518d4482299
Greg Look <greg@mvxcvi.com>
2021-12-26 15:20:49 -0800

    Implement json formatter.

--- src/clojure/dialog/format/json.clj
+++ src/clojure/dialog/format/json.clj
@@ -1,5 +1,34 @@
 (ns dialog.format.json
-  "Log format which presents events as JSON objects for structured logging.")
+  "Log format which presents events as JSON objects for structured logging."
+  (:require
+    [clojure.data.json :as json]
+    [io.aviso.exception :as ex]))
+
+
+(defn- sanitize-stack-frame
+  "Sanitize a map of stack frame data for JSON serialization."
+  [frame]
+  (into {}
+        (comp
+          (map (juxt identity frame))
+          (filter second))
+        [:class :file :line :method :name]))
+
+
+(defn- sanitize-exception
+  "Sanitize a map of exception data for JSON serialization."
+  [data]
+  (assoc data
+         :stack-trace
+         (mapv sanitize-stack-frame (:stack-trace data))))
+
+
+(defn- render-exception
+  "Convert an exception into a data structure suitable for serializing to JSON."
+  [ex]
+  (into []
+        (map sanitize-exception)
+        (ex/analyze-exception ex {})))
 
 
 (defn formatter
@@ -7,5 +36,7 @@
   [output]
   (fn format-event
     [event]
-    ;; TODO: implement
-    ,,,))
+    (json/write-str
+      (cond-> event
+        (:error event)
+        (update :error render-exception)))))
