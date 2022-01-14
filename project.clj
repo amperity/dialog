e07bf04ed3f4180e19b874e0431c4cebb0557dcc
Greg Look <greg@mvxcvi.com>
2022-01-14 10:23:54 -0800

    Bump SNAPSHOT version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.amperity/dialog "0.2.0"
+(defproject com.amperity/dialog "0.2.1-SNAPSHOT"
   :description "Logging library for integration with Ken and various outputs."
   :url "https://github.com/amperity/dialog"
   :license {:name "MIT License"
