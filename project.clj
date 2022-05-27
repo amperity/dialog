38431aa68a740408c565050b967a38136291f470
Greg Look <greg@mvxcvi.com>
2022-05-27 11:51:43 -0700

    Update dependencies, set SNAPSHOT version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.amperity/dialog "0.3.2"
+(defproject com.amperity/dialog "1.0.0-SNAPSHOT"
   :description "Logging library for integration with Ken and various outputs."
   :url "https://github.com/amperity/dialog"
   :license {:name "MIT License"
@@ -11,11 +11,11 @@
   [[lein-cloverage "1.2.2"]]
 
   :dependencies
-  [[org.clojure/clojure "1.10.3"]
+  [[org.clojure/clojure "1.11.1"]
    [org.clojure/data.json "2.4.0"]
-   [org.slf4j/slf4j-api "1.7.32"]
-   [org.slf4j/jul-to-slf4j "1.7.32"]
-   [org.slf4j/jcl-over-slf4j "1.7.32"]
+   [org.slf4j/slf4j-api "1.7.36"]
+   [org.slf4j/jul-to-slf4j "1.7.36"]
+   [org.slf4j/jcl-over-slf4j "1.7.36"]
    [io.aviso/pretty "1.1.1"]
    [aero "1.1.6"]]
 
@@ -33,11 +33,11 @@
 
   :profiles
   {:dev
-   {:dependencies [[org.clojure/tools.logging "1.2.3"]]}
+   {:dependencies [[org.clojure/tools.logging "1.2.4"]]}
 
    :repl
    {:source-paths ["dev"]
     :repl-options {:init-ns dialog.repl}
-    :dependencies [[org.clojure/tools.namespace "1.2.0"]]
+    :dependencies [[org.clojure/tools.namespace "1.3.0"]]
     :jvm-opts ["-XX:-OmitStackTraceInFastThrow"
                "-Ddialog.profile=repl"]}})
